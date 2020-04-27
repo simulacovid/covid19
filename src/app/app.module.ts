e1de20878658d147f064cb8679dc2248497b66f9
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-04-28 02:12:49 +1000

    routes added, bootstrap working and angular working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashComponent } from './pages/dash/dash.component';
+import { HowtouseComponent } from './pages/howtouse/howtouse.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ResearchersComponent } from './pages/researchers/researchers.component';
+import { VolunteersComponent } from './pages/volunteers/volunteers.component';
+import { MediaComponent } from './pages/media/media.component';
+import { NotesComponent } from './pages/notes/notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    DashComponent,
+    HowtouseComponent,
+    ArticlesComponent,
+    ResearchersComponent,
+    VolunteersComponent,
+    MediaComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/howtouse/howtouse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-howtouse',
+  templateUrl: './howtouse.component.html',
+  styleUrls: ['./howtouse.component.scss']
+})
+export class HowtouseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
