f92ab31f606dfb159537aea55ab6232a0dca810d
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-04-27 19:30:23 +1000

    starting project and trying to insert bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -12,7 +15,6 @@ import { NotesComponent } from './pages/notes/notes.component';
 import { DashComponent } from './pages/dash/dash.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    private toggleButton: any;
+    private sidebarVisible: boolean;
+    
+    constructor(public location: Location, private element : ElementRef) {
+        this.sidebarVisible = false;
+    }
+    
+    ngOnInit() {
+        const navbar: HTMLElement = this.element.nativeElement;
+        this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+    }
+    sidebarOpen() {
+        const toggleButton = this.toggleButton;
+        const html = document.getElementsByTagName('html')[0];
+        
+        setTimeout(function(){
+            toggleButton.classList.add('toggled');
+        }, 500);
+        html.classList.add('nav-open');
+        
+        this.sidebarVisible = true;
+    };
+    sidebarClose() {
+        const html = document.getElementsByTagName('html')[0];
+        this.toggleButton.classList.remove('toggled');
+        this.sidebarVisible = false;
+        html.classList.remove('nav-open');
+    };
+    sidebarToggle() {
+        if (this.sidebarVisible === false) {
+            this.sidebarOpen();
+        } else {
+            this.sidebarClose();
+        }
+    };
+    isHome() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        if(titlee.charAt(0) === '#'){
+            titlee = titlee.slice( 1 );
+        }
+        if( titlee === '/home' ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    isDocumentation() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        if(titlee.charAt(0) === '#'){
+            titlee = titlee.slice( 1 );
+        }
+        if( titlee === '/documentation' ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
