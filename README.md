79e0a66809902f7cd6f8ea27da6a66094281c349
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-04-28 01:55:37 +1000

    new navbar with toggling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Covid19
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.5.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app.routing';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HowtouseComponent } from './pages/howtouse/howtouse.component';
-import { ArticlesComponent } from './pages/articles/articles.component';
-import { ResearchersComponent } from './pages/researchers/researchers.component';
-import { VolunteersComponent } from './pages/volunteers/volunteers.component';
-import { MediaComponent } from './pages/media/media.component';
-import { NotesComponent } from './pages/notes/notes.component';
-import { DashComponent } from './pages/dash/dash.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HowtouseComponent,
-    ArticlesComponent,
-    ResearchersComponent,
-    VolunteersComponent,
-    MediaComponent,
-    NotesComponent,
-    DashComponent,
-    NavbarComponent,
-    FooterComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
