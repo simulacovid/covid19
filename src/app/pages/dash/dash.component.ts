e9c5542ee93db8983f65f00b8b6f4f880039f8cc
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-05-22 23:03:08 +1000

    datatable from hell

--- src/app/pages/dash/dash.component.ts
+++ src/app/pages/dash/dash.component.ts
@@ -13,20 +13,35 @@ export class DashComponent implements OnInit {
   ivc_rj = ivcovidRio;
   
   constructor() { }
-
-  public data = [
-    {name: 'therichpost', email: 'therichpost@gmail.com', website:'therichpost.com'},
-    {name: 'therichpost', email: 'therichpost@gmail.com', website:'therichpost.com'},
-    {name: 'therichpost', email: 'therichpost@gmail.com', website:'therichpost.com'},
-    {name: 'therichpost', email: 'therichpost@gmail.com', website:'therichpost.com'},
-];
-  title = 'angulardatatables';
   dtOptions: DataTables.Settings = {};
   ngOnInit() {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5,
-      processing: true
-    };
+      processing: true,
+      responsive: true,
+      language: {
+        processing: "Processando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ dados",
+        info: "Mostrando desde _START_ até _END_ de _TOTAL_ dados",
+        infoEmpty: "Mostrando nenhum dado.",
+        infoFiltered: "(filtrado _MAX_ dados total)",
+        infoPostFix: "",
+        loadingRecords: "Carregando...",
+        zeroRecords: "Não existem registros.",
+        emptyTable: "Não há dados para essa tabela.",
+        paginate: {
+          first: "Primeiro",
+          previous: "Anterior",
+          next: "Seguinte",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Ative para classificar a tabela em ordem crescente",
+          sortDescending: ": Ative para classificar a tabela em ordem decrescente"
+        }
+      }
+    }
   }
 }
